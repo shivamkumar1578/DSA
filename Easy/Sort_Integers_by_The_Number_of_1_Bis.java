56fd8f24808b746b80db72b3db511df7609481ad
Shivam Kumar <shivamkumarshinu985@gmail.com>
2026-02-25 10:29:14 +0530

    Solution of 1356

---
+++ Easy/Sort_Integers_by_The_Number_of_1_Bis.java
@@ -0,0 +1,23 @@
+public int[] sortByBits(int[] arr) {
+        Integer[] temp = new Integer[arr.length];
+
+        for (int i = 0; i < arr.length; i++) {
+            temp[i] = arr[i];
+        }
+
+        Arrays.sort(temp, (a, b) -> {
+            int countA = Integer.bitCount(a);
+            int countB = Integer.bitCount(b);
+
+            if (countA == countB) {
+                return a - b;
+            }
+            return countA - countB;
+        });
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = temp[i];
+        }
+
+        return arr;
+    }
