0b507de7ac2f697859ae9947e33c501c17889073
Shivam Kumar <shivamkumarshinu985@gmail.com>
2026-02-26 13:23:18 +0530

    Add LeetCode 1404 solution to Medium folder

---
+++ Medium/Number_of_Steps_to_Reduce_a_Number_in_Binary_Representation_to_One_1404.java
@@ -0,0 +1,23 @@
+package Medium;
+
+public class Number_of_Steps_to_Reduce_a_Number_in_Binary_Representation_to_One_1404 {
+    class Solution {
+    public int numSteps(String s) {
+        int steps = 0;
+        int carry = 0;
+
+        for (int i = s.length() - 1; i > 0; i--) {
+            int bit = s.charAt(i) - '0';
+
+            if (bit + carry == 1) {
+                steps += 2;
+                carry = 1;
+            } else {
+                steps += 1;
+            }
+        }
+
+        return steps + carry;
+    }
+}
+}
