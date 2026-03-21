e6198572ee62bfcd0972696ae6a0e2dadbf4cbbc
Shivam Kumar <shivamkumarshinu985@gmail.com>
2026-03-21 14:53:40 +0530

    Add Solution of 3567 (Easy)

---
+++ Easy/Flip_Square_Submatrix_Vertically_3643.java
@@ -0,0 +1,17 @@
+public class Flip_Square_Submatrix_Vertically_3643 {
+    public int[][] reverseSubmatrix(int[][] grid, int x, int y, int k) {
+        int top = x;
+        int bottom = x + k - 1;
+        
+        while (top < bottom){
+            for(int col = y; col < y + k; col++){
+                int temp = grid[top][col];
+                grid[top][col] = grid[bottom][col];
+                grid[bottom][col] = temp;
+            }
+            top++;
+            bottom--;
+        }
+        return grid;
+    }
+}
