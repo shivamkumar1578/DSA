42217f325be91bfc56e2c21ef9f1af8f8eddf7f9
Shivam Kumar <shivamkumarshinu985@gmail.com>
2026-04-05 21:31:29 +0530

    Add Solution of 657 (Easy)

---
+++ Easy/Robot_Return_to_Origin_657.java
@@ -0,0 +1,18 @@
+public class Robot_Return_to_Origin_657 {
+    public boolean judgeCircle(String moves) {
+        int x = 0, y = 0;
+
+        for (char c : moves.toCharArray()) {
+            if (c == 'U')
+                y++;
+            else if (c == 'D')
+                y--;
+            else if (c == 'R')
+                x++;
+            else
+                x--;
+        }
+
+        return x == 0 && y == 0;
+    }
+}
