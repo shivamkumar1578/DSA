0d3f12e9b26dbb533c2cd77dcee3c428029c2ff2
Shivam Kumar <shivamkumarshinu985@gmail.com>
2026-04-13 21:13:36 +0530

    Add Solution of 1848

---
+++ Easy/Minimum_Distance_to_the_Target_Element_1848.java
@@ -0,0 +1,13 @@
+public class Minimum_Distance_to_the_Target_Element_1848 {
+    public int getMinDistance(int[] nums, int target, int start) {
+        int minDist = Integer.MAX_VALUE;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
+                int dist = Math.abs(i - start);
+                minDist = Math.min(minDist, dist);
+            }
+        }
+        return minDist;
+    }
+}
