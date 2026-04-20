96e602f64625d71bef1ad5887a1a249565939d99
Shivam Kumar <shivamkumarshinu985@gmail.com>
2026-04-20 11:30:04 +0530

    Add Solution of 2078 (Easy)

---
+++ Easy/Two_Furthest_Houses_With_Different_Colors_2078.java
@@ -0,0 +1,15 @@
+public class Two_Furthest_Houses_With_Different_Colors_2078 {
+    static public int maxDistance(int[] colors) {
+        final int n = colors.length;
+        final int c0 = colors[0], cN = colors[n - 1];
+        int lMax = 0, rMax = 0;
+        for (int i = 0; i < n; i++) {
+            final int c = colors[i];
+            if (c0 != c && i > lMax)
+                lMax = i;
+            if (cN != c && n - 1 - i > rMax)
+                rMax = n - 1 - i;
+        }
+        return Math.max(lMax, rMax);
+    }
+}
