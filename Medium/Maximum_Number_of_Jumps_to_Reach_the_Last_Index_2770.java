68abce56a8141ba181b2004a83f87084f12286cd
Shivam Kumar <shivamkumarshinu985@gmail.com>
2026-05-10 16:17:48 +0530

    Add Solution of 2770 (Medium)

---
+++ Medium/Maximum_Number_of_Jumps_to_Reach_the_Last_Index_2770.java
@@ -0,0 +1,22 @@
+package Medium;
+
+import java.util.Arrays;
+
+public class Maximum_Number_of_Jumps_to_Reach_the_Last_Index_2770 {
+    public int maximumJumps(int[] nums, int target) {
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, -1);
+        dp[0] = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (i > 0 && dp[i] <= 0) {
+                continue;
+            }
+            for (int j = i + 1; j < nums.length; j++) {
+                if (Math.abs(nums[i] - nums[j]) <= Math.abs(target)) {
+                    dp[j] = Math.max(dp[j], dp[i] + 1);
+                }
+            }
+        }
+        return dp[nums.length - 1];
+    }
+}
