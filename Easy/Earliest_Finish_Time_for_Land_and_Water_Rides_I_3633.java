c826d277fff49385020324d7a688bbe66c40f9d5
Shivam Kumar <shivamkumarshinu985@gmail.com>
2026-06-02 21:48:55 +0530

    Solution of 3633 Easy

---
+++ Easy/Earliest_Finish_Time_for_Land_and_Water_Rides_I_3633.java
@@ -0,0 +1,23 @@
+class Earliest_Finish_Time_for_Land_and_Water_Rides_I_3633 {
+    class Solution {
+        public int earliestFinishTime(int[] landStartTime, int[] landDuration, int[] waterStartTime,
+                int[] waterDuration) {
+            int ans = Integer.MAX_VALUE;
+
+            for (int i = 0; i < landStartTime.length; i++) {
+                for (int j = 0; j < waterStartTime.length; j++) {
+
+                    int landFinish = landStartTime[i] + landDuration[i];
+                    int finish1 = Math.max(landFinish, waterStartTime[j]) + waterDuration[j];
+
+                    int waterFinish = waterStartTime[j] + waterDuration[j];
+                    int finish2 = Math.max(waterFinish, landStartTime[i]) + landDuration[i];
+
+                    ans = Math.min(ans, finish1);
+                    ans = Math.min(ans, finish2);
+                }
+            }
+            return ans;
+        }
+    }
+}
