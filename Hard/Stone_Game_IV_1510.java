81bc18b560acb5011cf35153dfd29dce2b8581a5
Shivam Kumar <shivamkumarshinu985@gmail.com>
2026-08-10 11:16:21 +0530

    Solution of 1510

---
+++ Hard/Stone_Game_IV_1510.java
@@ -0,0 +1,20 @@
+package Hard;
+
+public class Stone_Game_IV_1510 {
+    public boolean winnerSquareGame(int n) {
+        boolean dp[] = new boolean[n + 1];
+        for (int i = 0; i <= n; i++) {
+            if (!dp[i]) {
+                for (int j = 1; i + j * j <= n; j++) {
+                    dp[i + j * j] = true;
+                }
+
+                if (dp[n]) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+}
