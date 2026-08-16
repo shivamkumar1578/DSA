3edb0852d6dfb75249a5aaabc8f890073c464530
Shivam Kumar <shivamkumarshinu985@gmail.com>
2026-08-16 22:30:53 +0530

    Solution of 2029

---
+++ Medium/Stone_Game_IX_2029.java
@@ -0,0 +1,17 @@
+package Medium;
+
+public class Stone_Game_IX_2029 {
+    public boolean stoneGameIX(int[] stones) {
+        int[] count = new int[3];
+
+        for (int stone : stones) {
+            count[stone % 3]++;
+        }
+
+        if (count[0] % 2 == 0) {
+            return count[1] > 0 && count[2] > 0;
+        }
+
+        return Math.abs(count[1] - count[2]) > 2;
+    }
+}
